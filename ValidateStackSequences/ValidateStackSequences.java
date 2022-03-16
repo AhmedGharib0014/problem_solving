370b781992c49ff2f376fdacce67111095341eb7
Ahmed Gharib <ahmedgharib0014@gmail.com>
2022-03-17 01:14:12 +0200

    validate Stack Sequences

---
+++ ValidateStackSequences/ValidateStackSequences.java
@@ -0,0 +1,34 @@
+package ValidateStackSequences;
+
+import java.security.cert.TrustAnchor;
+import java.util.Stack;
+
+public class ValidateStackSequences {
+
+    public static boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int j = 0;
+
+        for (int i = 0; i < pushed.length; i++) {
+            stack.push(pushed[i]);
+            while (j < popped.length && !stack.empty() && popped[j] == stack.peek()) {
+                stack.pop();
+                j++;
+            }
+            if (j >= popped.length)
+                return true;
+
+        }
+
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        int[] popped = { 4, 3, 5, 1, 2 };
+        int[] pushed = { 1, 2, 3, 4, 5 };
+        System.out.println(validateStackSequences(pushed, popped));
+
+    }
+
+}
