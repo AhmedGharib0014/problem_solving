b20758b58766e80561efc39a9f5eaab729e3fa3e
Ahmed Gharib <ahmedgharib0014@gmail.com>
2022-04-01 10:38:03 +0200

    string to romain

---
+++ int_to_roman/solution.java
@@ -0,0 +1,22 @@
+public class solution {
+
+    public static String intToRoman(int num) {
+        String romanString="";
+	    int[] breakPoints = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romainChars={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+ 
+       for(int i=0;i<breakPoints.length;i++){
+           int x= num/breakPoints[i] ;
+           romanString =  romanString.concat(romainChars[i].repeat(x));
+           num -= (breakPoints[i]*x);
+       }
+        return romanString;
+        
+    }
+    
+
+
+    public static void main(String[] args) {
+        
+    }
+}
