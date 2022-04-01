28aa97b1e400d2feeca2135709e2af1fecd771e6
Ahmed Gharib <ahmedgharib0014@gmail.com>
2022-04-01 14:15:13 +0200

    three sum

---
+++ three_sum/solution.java
@@ -0,0 +1,44 @@
+public class solution {
+    
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result =new ArrayList<List<Integer>>();
+          Arrays.sort(nums);
+          for(int i=0;i<nums.length-2;i++){
+              int k=nums.length-1;
+              int j=i+1;
+              while(j < k){
+                  int sum=nums[i]+nums[j]+nums[k];
+                  if(sum == 0){
+                      List<Integer> temp= new ArrayList<Integer>();
+                      temp.add(nums[i]);
+                      temp.add(nums[j]);
+                      temp.add(nums[k]);
+                      result.add(temp);
+                      
+                     do{
+                          j++;
+                      }while(j < nums.length-1 && nums[j-1] == nums[j]) ;
+                      
+                  }else if(sum > 0){      
+                          k--;         
+                  }else{
+                          j++;
+                       
+                  }
+  
+              }
+              while(i < nums.length-1 && nums[i]==nums[i+1]){
+                  i++;
+              }
+              
+          }
+  
+  
+  
+              return  result;
+        
+          
+      }
+
+      
+}
