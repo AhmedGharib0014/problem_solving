fe42064ce1bffbb1e82611b61bd46e6b22d649ff
Ahmed Gharib <ahmedgharib0014@gmail.com>
2022-04-08 13:33:57 +0200

    binary tree level order traverse

--- bt_tree_zigzag_level_order_traversal/solution.java
+++ bt_tree_zigzag_level_order_traversal/solution.java
@@ -13,36 +13,34 @@ public class solution {
          }
         }
    
-       public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-           Stack<TreeNode> stack1= new  Stack<TreeNode>();
-           Stack<TreeNode> stack2= new  Stack<TreeNode>();
-           List<List<Integer>> ans = new  ArrayList<List<Integer>>();
-           if(root == null) return ans;
-           stack1.push(root);
-   
-           while (!stack1.empty() || !stack2.empty() ){
-               ArrayList<Integer> temp = new ArrayList<>();
-               if(!stack1.empty()){
-                   while (!stack1.empty()){
-                       TreeNode node = stack1.pop();
-                       if(node.left != null) stack2.add(node.left);
-                       if(node.right != null) stack2.add(node.right);
-                       temp.add(node.val);
-                       System.out.println(node.val);
-                   }
-               }else if(!stack2.empty()){
-                   while (!stack2.empty()){
-                       TreeNode node = stack2.pop();
-                       if(node.right != null) stack1.add(node.right);
-                       if(node.left != null) stack1.add(node.left);
-                       temp.add(node.val);
-                   }
-               }
-               if(temp.size()>0) ans.add(temp);
-   
-           }
-           return ans;
-   
-   
-       }
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            Stack<TreeNode> stack1= new  Stack<TreeNode>();
+             Stack<TreeNode> stack2= new  Stack<TreeNode>();
+             List<List<Integer>> ans = new  ArrayList<List<Integer>>();
+             if(root == null) return ans;
+             stack1.push(root);
+     
+             while (!stack1.empty() || !stack2.empty() ){
+                 ArrayList<Integer> temp = new ArrayList<>();
+                 if(!stack1.empty()){
+                     while (!stack1.empty()){
+                         TreeNode node = stack1.pop();
+                         if(node.left != null) stack2.add(node.left);
+                         if(node.right != null) stack2.add(node.right);
+                         temp.add(node.val);
+                     }
+                 }else if(!stack2.empty()){
+                     while (!stack2.empty()){
+                         TreeNode node = stack2.pop();
+                         if(node.right != null) stack1.add(node.right);
+                         if(node.left != null) stack1.add(node.left);
+                         temp.add(node.val);
+                     }
+                 }
+                 if(temp.size()>0) ans.add(temp);
+     
+             }
+             return ans;
+             
+         }
 }
