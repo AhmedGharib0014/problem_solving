3714979f36dba3748b10581bad52e8e1eb2e2a5f
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-08-31 22:59:42 +0200

    Time: 12 ms (6.05%), Space: 87.5 MB (5.01%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,34 +1,7 @@
 class Solution {
    
-    public  int maxSumArrayWidow(int[] nums){
-        if(nums.length == 0) return 0;
-
-        int max=Integer.MIN_VALUE;
-
-        for(int windowSize =1;windowSize <= nums.length;windowSize++){
-
-            int sum =0;
-
-            for(int j=0;j<windowSize;j++){
-                sum += nums[j];
-            }
-
-            if(sum>max)max=sum;
-
-            for(int j=windowSize;j<nums.length;j++) {
-                sum -= nums[j - windowSize];
-                sum += nums[j];
-                if(sum>max)max=sum;
-            }
-        }
-
-        return  max;
-    }
-
-   
-
-    public int maxSumKadanes(int[] nums){
-       ArrayList<Integer> arrayList = new ArrayList<>();
+    public int maxSubArray(int[] nums) {
+          ArrayList<Integer> arrayList = new ArrayList<>();
         int maxSum=Integer.MIN_VALUE;
         int sum=0;
 
@@ -47,9 +20,6 @@ class Solution {
         }
 
         return maxSum;
-    }
-    public int maxSubArray(int[] nums) {
-        return maxSumKadanes(nums);
     }
 
 
