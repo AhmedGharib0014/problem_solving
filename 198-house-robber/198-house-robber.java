3fe464975492cfc432b5e9334747703d3c34a2be
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-09-01 04:09:49 +0200

    Time: 0 ms (100.00%), Space: 41.5 MB (55.87%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -4,13 +4,10 @@ class Solution {
 
     public int rob(int[] nums) {
        if(nums.length==1) return nums[0];
-       int max1=nums[0];
-       int max2= Math.max(nums[0],nums[1]);
-
-       if(nums.length == 2) return max2;
+       int max1=0;
+       int max2= nums[0];
         
-      
-        for (int i=2;i<nums.length;i++){
+        for (int i=1;i<nums.length;i++){
             int temp = Math.max(nums[i]+max1,max2);
             max1=max2;
             max2=temp;
