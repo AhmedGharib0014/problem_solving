9cbc1e9903f43c932974b44e18131309eb3289c6
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-09-13 23:59:12 +0200

    Time: 6 ms (8.99%), Space: 41.9 MB (68.28%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+     public boolean isValid(String s) {
+        Stack<Character> characters=new Stack<Character>();
+        Set<Character> closing = Set.of(']','}',')');
+
+
+        for(int i=0;i<s.length();i++){
+            if(closing.contains(s.charAt(i))){
+                if(characters.empty()) return  false;
+                if(checkValidPair(characters.pop(),s.charAt(i)) ) continue;
+                else return false;
+            }else{
+                characters.push(s.charAt(i));
+            }
+        }
+        return characters.empty();
+    }
+
+
+    public boolean checkValidPair(Character openingChar , Character closingChar){
+        return ((openingChar=='(') && closingChar==')') ||((openingChar=='{') && closingChar=='}') || ((openingChar=='[') && closingChar==']');
+
+    }
+}
