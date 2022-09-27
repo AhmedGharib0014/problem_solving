6dc98431408eb7f892a6c054958984d325ab6416
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-09-28 00:20:05 +0200

    Time: 4 ms, Memory: 43 MB - LeetHub

---
+++ course-schedule/course-schedule.java
@@ -0,0 +1,52 @@
+class Solution {
+     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer,CourseNode> map = new HashMap<>();
+        for (int i = 0; i < numCourses; i++) {
+            map.put(i,new CourseNode(i));
+        }
+
+        for(int[] prerequisite : prerequisites){
+            map.get(prerequisite[1]).prerequisites.add(map.get(prerequisite[0]));
+        }
+
+
+        for (CourseNode node : map.values()){
+            if(node.visited==CourseNodeStatus.not_visited){
+                if(containsCycles(node)) return false;
+
+            }
+        }
+
+        return true;
+
+    }
+
+
+    public  boolean containsCycles(CourseNode node){
+        if(node.visited == CourseNodeStatus.visited) return false;
+        else  if(node.visited == CourseNodeStatus.processing) return true;
+        node.visited=CourseNodeStatus.processing;
+        for(CourseNode subNode : node.prerequisites){
+            if(containsCycles(subNode)) return true;
+        }
+        node.visited=CourseNodeStatus.visited;
+        return false;
+    }
+    
+    
+    enum CourseNodeStatus {not_visited,visited,processing}
+
+    class CourseNode{
+        int val ;
+        CourseNodeStatus visited;
+
+        List<CourseNode> prerequisites = new ArrayList<>();
+
+        public CourseNode(int val) {
+            this.val = val;
+            this.visited = CourseNodeStatus.not_visited;
+        }
+    }
+
+   
+}
