fb8871b0d4da157b1c515bf34db85838f9c96bbe
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-10-01 13:11:09 +0200

    Time: 376 ms (20.19%), Space: 118.3 MB (12.86%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,19 +1,41 @@
 class Solution {
     public String longestPalindrome(String s) {
-        for (int i = s.length(); i > 0 ; i--) {
-            for (int j = 0; j < s.length()-i; j++) {
-                String substring = s.substring(j, j + i+1);
-                if(isPalindrome(substring)) return substring;
+        int [][] dynamicArr =new int[s.length()][s.length()];
+        int start=0;
+        int end=0;
+
+
+        // fill diagonal  and next to diagonal
+        for(int i=0;i<s.length();i++){
+            dynamicArr[i][i]=1;
+
+            if(i<s.length()-1){
+                if( s.charAt(i)==s.charAt(i+1)){
+                    dynamicArr[i][i+1]=1;
+                    if((end-start)<1) {
+                        start=i;
+                        end=i+1;
+                    }
+                }
             }
-
         }
-        return s.substring(0,1);
+        
+        // fill other parts of the arr
+        // k os difference -->2,3,4,
+        for (int k = 2; k <= s.length(); k++) {
+            for (int j = 0; j+k <s.length() ; j++) {
+                if(s.charAt(j)==s.charAt(j+k) && dynamicArr[j+1][j+k-1] ==1){
+                    dynamicArr[j][j+k]=1;
+                    if(k >(end-start)){
+                        end=j+k;
+                        start=j;
+                    }
+                }
+            }
+        }
+
+        return s.substring(start,end+1);
     }
 
-    public boolean isPalindrome(String s){
-        for (int i = 0; i < s.length()/2; i++) {
-            if(s.charAt(i) != s.charAt(s.length()-i-1)) return false;
-        }
-        return true;
-    }
+
 }
