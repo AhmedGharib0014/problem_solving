44bb264eeb0ecf17b51af3183172e8e98113e50b
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-10-06 00:47:12 +0200

    Time: 16 ms (5.05%), Space: 48.8 MB (5.67%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -30,25 +30,21 @@ class Solution {
         while (!queue.isEmpty()){
            lenght=queue.size();
             Node current=null;
-            Node prev=null;
             for (int i = 0; i < lenght; i++) {
                 if(current == null){
                     current= queue.poll();
-                    if(current!= null){
-                        queue.add(current.left);
-                        queue.add(current.right);
-                    }
-                    continue;
+                   
+                }else {
+                    current.next= queue.poll();
+                    current=current.next;
                 }
-                prev= current;
-                current= queue.poll();
-               if(prev!= null) prev.next= current;
-               if(current!= null){
-                   queue.add(current.left);
-                   queue.add(current.right);
-               } 
+                
+                if(current!=null){
+                    queue.add(current.left);
+                    queue.add(current.right);
+                }
             }
-         if(current!= null)   current.next=null;
+            if(current!=null) current.next=null;
         }
          return root;
     }
