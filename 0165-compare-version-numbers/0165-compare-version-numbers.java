b1871e1b91b27abe3622b64cb081d40d48f5ddce
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-10-12 18:52:33 +0200

    Time: 1 ms (91.67%), Space: 41.6 MB (78.13%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,31 +1,33 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-         int v1 =0;
-        int v2=0;
-        int i=0;
-        int j=i;
-        
-        while (i<version1.length() || j<version2.length()){
-            
-            while (i<version1.length() && version1.charAt(i) != '.'){
-                v1=10*v1+(version1.charAt(i)-'0');
-                i++;
-            }
+        int revision1 =0;
+		int revision2=0;
+		
+		int index1=0;
+		int index2=0;
 
-            while (j<version2.length() && version2.charAt(j) != '.'){
-                v2=10*v2+(version2.charAt(j)-'0');
-                j++;
-            }
-            
-            if(v1>v2) return 1;
-            else if(v2>v1) return -1;
-            
-            v1=0;
-            v2=0;
-            i++;
-            j++;
-        }
-        
-        return 0;
+		while (index1<version1.length() || index2<version2.length()){
+
+			while (index1<version1.length() && version1.charAt(index1) != '.'){
+				revision1=10*revision1+(version1.charAt(index1)-'0');
+				index1++;
+			}
+
+			while (index2<version2.length() && version2.charAt(index2) != '.'){
+				revision2=10*revision2+(version2.charAt(index2)-'0');
+				index2++;
+			}
+
+			if(revision1>revision2) return 1;
+			else if(revision2>revision1) return -1;
+			
+			index1++;
+			index2++;
+
+			revision1=0;
+			revision2=0;
+		}
+
+		return 0;
     }
 }
