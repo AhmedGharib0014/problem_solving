3616079cacb3bd6b270df096ce7bc7a24f527fa6
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-10-14 14:47:18 +0200

    Time: 3 ms (11.09%), Space: 43.4 MB (49.68%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,44 @@
+class Solution {
+     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<ArrayList<Integer>> queue = new LinkedList<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+
+        for (int i = 0; i <nums.length ; i++) {
+            map.put(nums[i],i);
+        }
+
+        queue.add(new ArrayList<>());
+
+        while (!queue.isEmpty()){
+            int size = queue.size();
+
+
+
+            for (int i = 0; i < size; i++) {
+
+                ArrayList<Integer> temp = queue.poll();
+                list.add(temp);
+                int start=0;
+                if(temp.size()>0){
+                    start= map.getOrDefault(temp.get(temp.size()-1),0)+1;
+                }
+
+                for (int j =start; j < nums.length ; j++) {
+                    ArrayList<Integer> temp2 = new ArrayList<>(temp);
+                    temp2.add(nums[j]);
+                    queue.add(temp2);
+                }
+
+            }
+
+        }
+
+        return list;
+    }
+
+
+
+
+}
