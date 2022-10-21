f622746b26f0f8ca191dd012430d0cba4889cd9b
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-10-21 08:21:27 +0200

    Time: 1 ms (80.94%), Space: 43.3 MB (5.10%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -33,7 +33,7 @@ class Solution {
     public int binarySearch(int[] nums , int start , int end , int target){
         if (start > end) return -1;
 
-        int mid= (start + end)/2;
+        int mid= (start + end) / 2;
 
         if(nums[mid] == target){
             return mid;
