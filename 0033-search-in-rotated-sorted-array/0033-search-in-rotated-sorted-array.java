e27ce196ff44d6da460d6855dd478274aa7e43dd
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-10-21 08:00:57 +0200

    Time: 1 ms (80.94%), Space: 42.4 MB (66.83%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,43 @@
+class Solution {
+      public int search(int[] nums, int target) {
+        int end = nums.length-1;
+        int rotationKey = findRotationKey(nums,0,end);
+
+        int index = binarySearch(nums,0,rotationKey,target);
+
+        if(index== -1 )
+            index=binarySearch(nums,rotationKey+1,end,target);
+
+
+        return index;
+
+    }
+
+    public int findRotationKey(int[] nums,int start , int end){
+        if(start>end) return end;
+
+        int mid= (start+end)/2;
+
+        if(nums[mid] >= nums[0])
+            return findRotationKey(nums,mid+1,end);
+
+        else
+           return findRotationKey(nums,start,mid-1);
+
+    }
+
+    public int binarySearch(int[] nums , int start , int end , int target){
+        if (start>end)
+            return -1;
+
+        int mid= (start+end)/2;
+
+        if(nums[mid] == target) return mid;
+        else if( nums[mid] > target )
+            return binarySearch(nums,start,mid-1,target);
+        else
+            return binarySearch(nums,mid+1,end,target);
+    }
+
+
+}
