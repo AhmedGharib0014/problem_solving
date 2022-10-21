54d20cf8f96843d251e41fae90849827282a9d93
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-10-21 08:16:45 +0200

    Time: 1 ms (80.94%), Space: 43.1 MB (11.23%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,15 +1,15 @@
 class Solution {
     
    public int search(int[] nums, int target) {
-        
-        int end = nums.length-1;
-        
-        int rotationKey = findRotationKey(nums,0,end);
 
-        int index = binarySearch(nums,0,rotationKey,target);
+        int end = nums.length-1;
+
+        int rotationKey = findRotationKey(nums, 0, end);
+
+        int index = binarySearch(nums, 0, rotationKey, target);
 
         if(index== -1 ){
-            index=binarySearch(nums,rotationKey+1,end,target);
+            index=binarySearch(nums, rotationKey+1, end, target);
         }
 
         return index;
@@ -17,16 +17,17 @@ class Solution {
     }
 
     public int findRotationKey(int[] nums,int start , int end){
-        
+
         if(start> end) return end;
 
         int mid= (start + end)/ 2;
 
         if(nums[mid] >= nums[0]) {
-            return findRotationKey(nums,mid + 1,end);
+            return findRotationKey(nums, mid + 1, end);
         } else{
-            return findRotationKey(nums,start,mid - 1);
+            return findRotationKey(nums, start, mid - 1);
         }
+        
     }
 
     public int binarySearch(int[] nums , int start , int end , int target){
@@ -37,12 +38,11 @@ class Solution {
         if(nums[mid] == target){
             return mid;
         } else if( nums[mid] > target ){
-            return binarySearch(nums,start,mid-1,target);
+            return binarySearch(nums, start, mid-1, target);
         } else{
-            return binarySearch(nums,mid+1,end,target);
+            return binarySearch(nums, mid + 1, end, target);
         }
-            
-    }
 
+    }
 
 }
