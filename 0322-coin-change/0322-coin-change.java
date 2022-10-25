fb16f17655fdbb6c44c54d26bee62020a55ab762
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-10-25 05:49:22 +0200

    Time: 33 ms (52.63%), Space: 45 MB (58.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,6 +2,8 @@ class Solution {
      public int coinChange(int[] coins, int amount) {
 
         int[] arr = new int[amount+1];
+         
+         arr[0]=0;
 
         for (int i = 1; i <= amount ; i++) {
             arr[i] = calculateMinChange(arr,coins,i);
@@ -12,12 +14,14 @@ class Solution {
 
     private int calculateMinChange(int[] arr, int[] coins, int i) {
         int minNumberOfCoins = Integer.MAX_VALUE;
+        
         for (int coin : coins){
+            
             int temp=i-coin;
-            if (temp == 0) return 1;
-            if(temp>0 && arr[temp] != -1){
+            if(temp >= 0 && arr[temp] != -1){
                 minNumberOfCoins=Math.min(arr[temp]+1,minNumberOfCoins);
             }
+            
         }
 
        return minNumberOfCoins != Integer.MAX_VALUE? minNumberOfCoins:-1;
