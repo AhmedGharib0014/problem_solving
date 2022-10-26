e9f151b0a0623d9d14e4fabadaba3aeb615ece25
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-10-26 20:28:00 +0200

    Time: 228 ms (12.47%), Space: 54.2 MB (12.31%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        Map<Integer,ArrayList<Integer>> map = new HashMap<>();
+        int max=1;
+
+        
+        if(nums.length<1) return 0;
+        if(nums.length <2) return 1;
+        else {
+            ArrayList<Integer> tem= new ArrayList<Integer>();
+            tem.add(nums[0]);
+            map.put(max,tem);
+
+        }
+
+
+        for (int i = 1; i < nums.length; i++) {
+
+            int tempMax= max;
+            while (tempMax>0){
+                ArrayList<Integer> list = map.get(tempMax);
+                boolean done = false;
+                for (int j : list){
+                    if(nums[i]>j){
+                        ArrayList<Integer> list2=  map.getOrDefault(tempMax+1,new ArrayList<Integer>());
+                        list2.add(nums[i]);
+                        map.put(tempMax+1,list2);
+                        if(tempMax+1>max)max=tempMax+1;
+                        done = true;
+                        break;
+                    }
+                }
+                if(done) break;
+                else tempMax--;
+            }
+
+            if (tempMax<=0){
+                ArrayList<Integer> list2=  map.get(1);
+                list2.add(nums[i]);
+            }
+        }
+
+        return max;
+    }
+}
