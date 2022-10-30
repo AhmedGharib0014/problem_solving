be4155a85dbd56e1dcdbf953bebf394450f54ea8
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-10-30 22:10:29 +0200

    Time: 305 ms (7.91%), Space: 54.6 MB (5.11%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int jump(int[] nums) {
+        int[] maxJums = new int[nums.length];
+       
+
+
+        for (int i = maxJums.length-2; i >= 0 ; i--) {
+            
+            if(nums.length -1  <= i+nums[i]) maxJums[i]=1;
+            else {
+                int min = Integer.MAX_VALUE;
+                for (int j = i+1; j < Math.min(i+nums[i]+1,nums.length)  ; j++) {
+                    if(maxJums[j]>0 && maxJums[j] < min) min= maxJums[j];
+                }
+                if(min == Integer.MAX_VALUE) nums[i]= -1;
+                else maxJums[i] = min+1;
+                
+                System.out.println(maxJums[i]);
+            }
+            
+        }
+        
+        
+        return maxJums[0];
+    }
+}
