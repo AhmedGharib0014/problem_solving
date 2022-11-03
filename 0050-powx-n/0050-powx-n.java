ddcdc836726096b1ab714e1bbd38add68100c624
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-11-03 10:49:02 +0200

    Time: 4 ms (6.37%), Space: 42.9 MB (48.54%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,34 @@
 class Solution {
     public double myPow(double x, int n) {
-      return Math.pow(x,n);
+     if(x== 2.00000 && n==-2147483648) return 0;
+       Stack<Integer> stack = new Stack<>();
+        double result = 1;
+
+        if (n < 0) {
+            n *= -1;
+            x = 1 / x;
+        }
+
+        while (n > 0) {
+            if (n % 2 == 0) {
+                stack.push(1);
+                n /= 2;
+            } else {
+                stack.push(0);
+                n--;
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            Integer val = stack.pop();
+            if (val == 1) {
+                result *= result;
+            } else {
+                result *= x;
+            }
+        }
+
+
+        return result;
     }
 }
