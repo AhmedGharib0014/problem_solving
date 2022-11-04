6d6a5d2dc0e6bf20cac89f99c341d8c9cb9672b1
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-11-04 19:06:16 +0200

    Time: 3 ms (53.28%), Space: 41.7 MB (24.83%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int mySqrt(int x) {
+       int result = binarySearch(0, x, x);
+        if (result * result > x) {
+            return result - 1;
+        } else {
+            return result;
+        }
+
+    }
+
+    int binarySearch(int start, int end, int x) {
+        if (start >= end) {
+            return end;
+        }
+
+        int mid = (start + end) / 2;
+        double midsqr = (double) mid * mid * 1.0;
+
+        if (midsqr == x) {
+            return mid;
+        } else if (midsqr > x) {
+            return binarySearch(start, mid - 1, x);
+        } else {
+            return binarySearch(mid + 1, end, x);
+        }
+    }
+
+
+}
