37934b6117d41102e8a7b4280d864a3baaedb6ac
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-12-02 19:59:17 +0200

    Time: 0 ms (100.00%), Space: 41.7 MB (62.88%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countSegments(String s) {
+         if (s.isEmpty()) {
+            return 0;
+        }
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                count++;
+                while (i < s.length() && s.charAt(i) != ' ') {
+                    i++;
+                }
+            }
+        }
+
+
+        return count;
+    }
+}
