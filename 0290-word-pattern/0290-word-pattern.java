a52e683e120a0b213718b4d86b5d38a4b37d754f
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2022-12-03 13:00:24 +0200

    Time: 2 ms (47.50%), Space: 41.9 MB (57.10%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,28 +1,31 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-                    
-
         HashMap<Character, String> map = new HashMap<>();
         HashSet<String> values = new HashSet<>();
         StringBuilder builder = new StringBuilder();
         String[] arr = s.split(" ");
-         if(pattern.length() != arr.length) return false;
         
-
+        if (pattern.length() != arr.length) return false;
+        
         int index = 0;
         for (Character a : pattern.toCharArray()) {
             if (!map.containsKey(a)) {
-               if(values.contains( arr[index])) return false;
+                
+                if (values.contains(arr[index])) {
+                    return false;
+                }else {
+                    values.add(arr[index]);
+                }
+                
                 map.put(a, arr[index]);
-                values.add(arr[index]);
+              
             }
             builder.append(map.get(a));
             builder.append(" ");
             index++;
         }
-        System.out.println(builder.toString());
-
-
+        
         return builder.toString().trim().equals(s);
     }
+
 }
