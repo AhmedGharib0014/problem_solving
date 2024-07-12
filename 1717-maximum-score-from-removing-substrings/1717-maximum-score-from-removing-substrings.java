7c104ec4a9e4952c7049d2943571fd519e9a799d
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2024-07-12 12:23:53 +0300

    Time: 107 ms (61.62%), Space: 45.7 MB (62.63%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
@@ -1,25 +1,17 @@
 class Solution {
-      public int maximumGain(String s, int x, int y) {
-
+     public int maximumGain(String s, int x, int y) {
         int result = 0;
-        
         StringBuilder stringBuilder = new StringBuilder(s);
+        
         if (x > y) {
-            result += calculateForX(stringBuilder, x);
-            StringBuilder stringBuilder2 = new StringBuilder();
-            for (int i = 0; i < stringBuilder.length(); i++) {
-                if (stringBuilder.charAt(i) != '*') stringBuilder2.append(stringBuilder.charAt(i));
-            }
-            result += calculateForY(stringBuilder2, y);
-
+            result += calculateMaxGain(stringBuilder, 'a', 'b', x);
+            stringBuilder = removeAstrix(stringBuilder);
+            result += calculateMaxGain(stringBuilder, 'b', 'a', y);
 
         } else {
-            result += calculateForY(stringBuilder, y);
-            StringBuilder stringBuilder2 = new StringBuilder();
-            for (int i = 0; i < stringBuilder.length(); i++) {
-                if (stringBuilder.charAt(i) != '*') stringBuilder2.append(stringBuilder.charAt(i));
-            }
-            result += calculateForX(stringBuilder2, x);
+            result += calculateMaxGain(stringBuilder, 'b', 'a', y);
+            stringBuilder = removeAstrix(stringBuilder);
+            result += calculateMaxGain(stringBuilder, 'a', 'b', x);
 
         }
 
@@ -28,51 +20,35 @@ class Solution {
 
     }
 
-    private int calculateForX(StringBuilder stringBuilder, int x) {
-        int result = 0;
-        String s = stringBuilder.toString();
-        Stack<Integer> aStach = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'a') aStach.push(i);
-            else if (s.charAt(i) == 'b') {
-                if (!aStach.isEmpty()) {
-                    stringBuilder.setCharAt(aStach.pop(), '*');
-                    stringBuilder.setCharAt(i, '*');
-                    result += x;
-                }
-
-            } else {
-                aStach.clear();
-            }
-
+    private static StringBuilder removeAstrix(StringBuilder stringBuilder) {
+        StringBuilder stringBuilder2 = new StringBuilder();
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            if (stringBuilder.charAt(i) != '*') stringBuilder2.append(stringBuilder.charAt(i));
         }
-
-        return result;
+        return stringBuilder2;
     }
 
-
-    private int calculateForY(StringBuilder stringBuilder, int y) {
+    private int calculateMaxGain(StringBuilder stringBuilder, Character first, Character second, int y) {
         int result = 0;
-        String s = stringBuilder.toString();
-        Stack<Integer> bStach = new Stack<>();
+        Stack<Integer> firstCharStack = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'b') bStach.push(i);
-            else if (s.charAt(i) == 'a') {
-                if (!bStach.isEmpty()) {
-                    stringBuilder.setCharAt(bStach.pop(), '*');
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            if (stringBuilder.charAt(i) == first) firstCharStack.push(i);
+            else if (stringBuilder.charAt(i) == second) {
+                if (!firstCharStack.isEmpty()) {
+                    stringBuilder.setCharAt(firstCharStack.pop(), '*');
                     stringBuilder.setCharAt(i, '*');
                     result += y;
                 }
 
             } else {
-                bStach.clear();
+                firstCharStack.clear();
             }
 
         }
 
         return result;
     }
+
 
 }
