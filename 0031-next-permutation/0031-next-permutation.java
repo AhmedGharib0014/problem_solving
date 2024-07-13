d2ee0170898fb7121c2900c10c1aa8edc4879f53
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2024-07-13 22:27:24 +0300

    Time: 1 ms (20.72%), Space: 42.8 MB (80.23%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,31 +1,38 @@
 class Solution {
-     public void nextPermutation(int[] nums) {
-         if (nums.length < 2) return;
-
+    
+    public void nextPermutation(int[] nums) {
+        if (nums.length < 2) return;
         int previousValue = nums[nums.length - 1];
 
         for (int i = nums.length - 2; i >= 0; i--) {
             if (nums[i] < previousValue) {
-                int j = i + 1;
-                while (j < nums.length && nums[i] >= nums[j]) j++;
-                swap(nums, i, j);
+                swapTheValueWithTheFirstGreaterOne(nums, i);
                 break;
             } else {
                 previousValue = nums[i];
-                int j = i;
-                while (j + 1 < nums.length && nums[j] > nums[j + 1]) {
-                    swap(nums, j, j + 1);
-                    j++;
-                }
+                moveTheValueToTheCorrectOrder(nums, i);
             }
         }
     }
 
-    private static void swap(int[] nums, int i, int number) {
-        int temp = nums[i];
-        nums[i] = nums[number];
-        nums[number] = temp;
+    private void swapTheValueWithTheFirstGreaterOne(int[] nums, int i) {
+        int j = i + 1;
+        while (j < nums.length && nums[i] >= nums[j]) j++;
+        swap(nums, i, j);
     }
 
+    private void moveTheValueToTheCorrectOrder(int[] nums, int i) {
+        int j = i;
+        while (j + 1 < nums.length && nums[j] > nums[j + 1]) {
+            swap(nums, j, j + 1);
+            j++;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 
 }
