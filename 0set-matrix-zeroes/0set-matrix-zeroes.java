4c3c353ee983e452e49c04a8c53fd5b6626c2c30
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2024-09-06 16:36:04 +0300

    Time: 1 ms, Memory: 45.3 MB - LeetHub

--- 0set-matrix-zeroes/0set-matrix-zeroes.java
+++ 0set-matrix-zeroes/0set-matrix-zeroes.java
@@ -1,22 +1,55 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        HashSet<Integer> rowsIndex=new HashSet<Integer>();
-        HashSet<Integer> columnIndex=new HashSet<Integer>();
-        for (int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    rowsIndex.add(i);
-                    columnIndex.add(j);
+       boolean firstRow = false;
+        boolean firstColumn = false;
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstRow = true;
+                break;
+            }
+        }
+
+        for (int j = 0; j < columns; j++) {
+            if (matrix[0][j] == 0) {
+                firstColumn = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for (int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(rowsIndex.contains(i)|| columnIndex.contains(j)){
-                    matrix[i][j]=0;
+
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
                 }
-               
             }
         }
+
+        if (firstRow) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (firstColumn) {
+            for (int j = 0; j < columns; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
     }
 }
