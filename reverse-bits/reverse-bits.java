a07112071b815dac849ec74f27782276c5c66605
ahmed Gharib <35800420+AhmedGharib0014@users.noreply.github.com>
2024-11-03 10:10:21 +0200

    Time: 0 ms, Memory: 41.8 MB - LeetHub

--- reverse-bits/reverse-bits.java
+++ reverse-bits/reverse-bits.java
@@ -10,36 +10,7 @@ public class Solution {
             result |= reversed;
             n >>= 1;
         }
-        result >>= 0;
 
         return result;
-    }
-
-    public int swap(int n, int i) {
-        int x = getBitOfI(n, (31 - i));
-        int y = getBitOfI(n, i);
-
-        n = manipulateBitOfX(n, i, x);
-        n = manipulateBitOfX(n, (31 - i), y);
-
-        return n;
-    }
-
-    private static int manipulateBitOfX(int n, int i, int x) {
-        if (x != 0) {
-            n = n | (1 << i);
-        } else {
-            n = n & (~(1 << i));
-        }
-        return n;
-    }
-
-    private static int getBitOfI(int n, int i) {
-        int x = 1 << i;
-        return (n & x);
-    }
-
-
-   
-
+    }   
 }
